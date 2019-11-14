43e65944e49c39a9d0001880725c83191032474a
Jay Wen <jaywen89@gmail.com>
2019-11-14 20:48:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,5 +39,8 @@ def map_to_square(source_array)
   return new
 end
       
-def reduce_to_total(source_array, starting_point)      
+def reduce_to_total(source_array, starting_point)  
+  new = []
+  i = 0
+  while i < 
   
